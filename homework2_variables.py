47029d7b828744a93db3a07ce09c7273b9a55a85
Roman-Machtakov <79520337274@mail.ru>
2024-06-19 10:31:16 +0300

    Add files via upload

---
+++ homework2_variables.py
@@ -0,0 +1,26 @@
+#Домашнее задание
+
+#Количество домашних заданий
+number_of_homework = 12
+numberOfHomework = 12
+
+#Количество затраченных часов
+number_of_hours = 1.5
+numberOfHours = 1.5
+
+#Назавание курса
+course_name = "Python"
+courseName = "Python"
+
+#Время на одно задание
+time_for_one_task = number_of_hours / number_of_homework
+timeForOneTask = numberOfHours / numberOfHomework
+
+#Вывод в консоль строки
+#Варинт 1:
+print(course_name + ", всего задач: " + str(number_of_homework) + ", затрачено часов : " + str(number_of_hours) + ", среднее время выполнения : " + str(time_for_one_task)+" часа.") 
+#Вариант 2:
+print(courseName + ", всего задач: " + str(numberOfHomework) + ", затрачено часов : " + str(numberOfHours) + ", среднее время выполнения : " + str(timeForOneTask) + " часа.")
+
+
+
