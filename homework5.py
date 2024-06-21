f846a7cee741e9648bfa1dd4498e85fdd62dd699
Roman-Machtakov <79520337274@mail.ru>
2024-06-21 12:37:05 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,7 +1,7 @@
-immutable_var = 1, 2, True, "String"#Присвоение переменной immutable_var кортежа
-print(immutable_var)#Вывод в консоль кортеж immutable_var
-#immutable_var[1] = 3
-#print(immutable_var) Объект кортежа не поддерживает назначение элементов
+immutable_var = 1, 2, True, "String" #Присвоение переменной immutable_var кортежа
+print(immutable_var) #Вывод в консоль кортеж immutable_var
+#immutable_var[1] = 3 #Изменение элемента кортежа по индексу
+#print(immutable_var) #Объект кортежа не поддерживает изменение элементов
 
 
 mutable_list = ["apple", 4, 3.56, True]#Присвоение переменной mutable_list списка 
